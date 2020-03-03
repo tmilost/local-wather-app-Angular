8c1277604f42c628b97f5dd30b49d2ff48f608bb
Milos Terzic <46928715+tmilost@users.noreply.github.com>
2020-03-03 12:11:46 +0100

    interaction

--- src/app/weather/weather.service.ts
+++ src/app/weather/weather.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
-import { Observable } from 'rxjs'
+import { BehaviorSubject, Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { environment } from '../../environments/environment'
 import { ICurrentWeather } from '../interfaces'
@@ -29,14 +29,47 @@ export interface IWeatherService {
 @Injectable({
   providedIn: 'root',
 })
-export class WeatherService implements IWeatherService{
+export class WeatherService implements IWeatherService {
+  currentWeather = new BehaviorSubject<ICurrentWeather>({
+    city: '--',
+    country: '--',
+    date: Date.now(),
+    image: '',
+    temperature: 0,
+    description: '',
+  })
+
   constructor(private httpClient: HttpClient) {}
 
-  getCurrentWeather(city: string, country: string): Observable<ICurrentWeather> {
+  getCurrentWeather(
+    search: string | number,
+    country?: string
+  ): Observable<ICurrentWeather> {
+    let uriParams = ''
+    if (typeof search === 'string') {
+      uriParams = `q=${search}`
+    } else {
+      uriParams = `zip=${search}`
+    }
+
+
+    if (country) {
+      uriParams = `${uriParams},${country}`
+    }
+
+    return this.getCurrentWeatherHelper(uriParams)
+  }
+
+  getCurrentWeatherByCoords(coords: Coordinates): Observable<ICurrentWeather> {
+    const uriParams = `lat=${coords.latitude}&lon=${coords.longitude}`
+    return this.getCurrentWeatherHelper(uriParams)
+  }
+
+  private getCurrentWeatherHelper(uriParams: string): Observable<ICurrentWeather> {
     return this.httpClient
       .get<ICurrentWeatherData>(
         `${environment.baseUrl}api.openweathermap.org/data/2.5/weather?` +
-          `q=${city},${country}&appid=${environment.appId}`
+          `${uriParams}&appid=${environment.appId}`
       )
       .pipe(map(data => this.transformToICurrentWeather(data)))
   }
@@ -46,13 +79,13 @@ export class WeatherService implements IWeatherService{
       city: data.name,
       country: data.sys.country,
       date: data.dt * 1000,
-      image: `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
+      image: `http://openweathermap.org/img/w/${data.weather[0].icon}.png`,
       temperature: this.convertKelvinToFahrenheit(data.main.temp),
       description: data.weather[0].description,
     }
   }
 
   private convertKelvinToFahrenheit(kelvin: number): number {
-    return  kelvin - 273.15
+    return kelvin * 9 / 5 - 459.67
   }
 }
